18a4e37d682f39fc54ab1409a07af53600700264
samouraei <168327198+samouraei@users.noreply.github.com>
2024-09-09 13:24:03 +0330

    admin signup added

--- app.js
+++ app.js
@@ -1,47 +1,36 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
+const fs = require('fs');
 const app = express();
-const morgan = require("morgan");
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const hpp = require("hpp");
-const AppError = require("./utils/appError");
-const globalErrorHandler = require("./controllers/errorController");
-const userRouter = require("./routes/mainRoute");
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const userRouter = require('./routes/mainRoute');
 // const activationRouter = require('./routes/userActivationRoute'); // Ensure this file exists
 // const createProfileRouter = require('./routes/createProfileRoute'); // Ensure this file exists
-
-// Manually add headers for CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
 
 // Set security HTTP headers
 app.use(helmet());
 
 // Development logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
 }
 
 // Limit requests from the same IP
 const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many requests from this IP, please try again in an hour.",
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour.'
 });
-app.use("/api", limiter);
+app.use('/api', limiter);
 
 // Body parser reading data from body into req.body
-app.use(express.json({ limit: "10kb" }));
+app.use(express.json({ limit: '10kb' }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -50,33 +39,31 @@ app.use(mongoSanitize());
 app.use(xss());
 
 // Prevent parameter pollution
-app.use(
-  hpp({
+app.use(hpp({
     whitelist: [
-      "duration",
-      "ratingsQuantity",
-      "ratingsAverage",
-      "maxGroupSize",
-      "difficulty",
-      "price",
-    ],
-  })
-);
+        'duration',
+        'ratingsQuantity',
+        'ratingsAverage',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+    ]
+}));
 
 // Test middleware
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
+    req.requestTime = new Date().toISOString();
+    next();
 });
 
 // Routes
-app.use("/api/v1/web", userRouter);
+app.use('/api/v1/web', userRouter);
 // app.use('/api/v1/web/user/activation', activationRouter); // Use activationRouter here
 // app.use('/api/v1/web/user/createProfile', createProfileRouter); // Use createProfileRouter here
 
 // Handle undefined routes
-app.all("*", (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
 // Global error handler
--- routes/admin/view.js
+++ routes/admin/view.js
@@ -1,27 +1,32 @@
 const express = require('express');
 const visaController = require('../../controllers/visaController');
+const adminController = require('../../controllers/adminController');
 const jwtAuthService = require('../../services/jwtAuthService');
 const sanitizeVisaData = require('../../middlewares/visa/sanitizeVisaData')
 const router = express.Router();
 // const sanitizeVerificationCode  = require('../../middlewares/userMiddlewares/sanitizeVerificationCode');
 
+router.post('/signup',
+    jwtAuthService.protect,
+    adminController.adminSignUp);
+
 
 router.get('/visa/findCountry',
     jwtAuthService.protect,
-    jwtAuthService.restrictTo('user'),
+    jwtAuthService.restrictTo('admin'),
     visaController.findCountry
 );
 
 router.post('/visa/setVisaKind',
     jwtAuthService.protect,
-    jwtAuthService.restrictTo('user'),
+    jwtAuthService.restrictTo('admin'),
     visaController.setVisaKind
 );
 
 router.post('/visa/createVisa',
     sanitizeVisaData,
     jwtAuthService.protect,
-    jwtAuthService.restrictTo('user'),
+    jwtAuthService.restrictTo('admin'),
     visaController.createVisa
 );
 
